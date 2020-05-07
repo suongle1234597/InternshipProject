f38a5bd27e38ff93a80bc8ebda19328f671a76c6
suongle12397 <suongle12397@gmail.com>
2020-05-07 15:32:13 +0700

    update product detail

--- src/reducer/productReducer.js
+++ src/reducer/productReducer.js
@@ -7,11 +7,11 @@ const initialState = {
 
 const productReducer = (state = initialState, action) => {
     switch (action.type) {
-        // case GET_PRODUCT: {
-        //     return {
-        //         product: action.response
-        //     }
-        // }
+        case GET_PRODUCT: {
+            return {
+                product: action.response
+            }
+        }
         // case GET_PRODUCT_DETAIL: {
         //     return {
         //         product: action.response
